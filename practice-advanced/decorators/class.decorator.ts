de30da528aa8b918c94a838806a30702d6a0b045
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-05-17 00:07:13 +0900

    feat: classデコレーター実装

---
+++ practice-advanced/decorators/class.decorator.ts
@@ -0,0 +1,34 @@
+/**
+ * Typescriptにおいてデコレーターは関数形式で作成する。
+ * e.g. function classDecorator(constructor: Function){・・・}
+ * 引数としてコンストラクター関数を受け取る。
+ */
+
+/**
+ * newMsgプロパティとoverMsgプロパティの初期化をするクラスデコレーター
+ * @param {new(...args: any[])} インスタンス生成時におけるコンストラクター関数を表す。
+ *
+ */
+function msgDeco<T extends { new (...args: any[]): {} }>(constructor: T) {
+  return class extends constructor {
+    newMsg = 'new message' // 新しくnewMsgのプロパティが追加される
+    overMsg = 'deco message' // 値が上書きされる。
+  }
+}
+
+@msgDeco
+class MyClass {
+  myMsg: string
+  overMsg: string
+
+  constructor(myMsg: string, overMsg: string) {
+    this.myMsg = myMsg
+    this.overMsg = overMsg
+  }
+}
+
+console.log(new MyClass('my message', 'over message'))
+
+// { myMsg: 'my message',
+//   overMsg: 'deco message',
+//   newMsg: 'new message' }
