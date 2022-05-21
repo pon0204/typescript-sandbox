ae57362a3a6c9a03bd34d0b6eaff6bc9fcdb91e3
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-05-21 12:05:37 +0900

    chore: uniq追加テスト

--- practice-loadsh/src/uniq/value.ts
+++ practice-loadsh/src/uniq/value.ts
@@ -5,3 +5,7 @@ export const array: number[] = [1, 2, 1]
 
 export const MyValue = MyUniq<number>(array)
 export const lodashValue = uniq<number>(array)
+
+// export const array: (string | number)[] = [1, '伊藤', 1]
+// export const MyValue = MyUniq<string | number>(array)
+// export const lodashValue = uniq<string | number>(array)
