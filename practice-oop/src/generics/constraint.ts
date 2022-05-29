745e8853a7094dac6c86d495a9e18eb8e73451a2
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-05-29 22:16:20 +0900

    feat: class generics constrains

---
+++ practice-oop/src/generics/constraint.ts
@@ -0,0 +1,25 @@
+/**
+ * Typescriptは「構造関係」で互換性を判断する。
+ * Otherには、Parentと同じくnameプロパティがあるのでご完成があるとみなされる。
+ */
+
+class Parent {
+  name = 'Parent'
+}
+
+class Child extends Parent {
+  code = 'Child'
+}
+
+class Other {
+  name = 'Other' // Parentと同じくnameプロパティがある
+  flag = true
+}
+
+function genericsFn<T extends Parent>(arg: T) {
+  console.log(arg)
+}
+
+genericsFn(new Parent())
+genericsFn(new Child())
+genericsFn(new Other()) // 継承されいないが、nameプロパティを持っているのでエラー起きない。
