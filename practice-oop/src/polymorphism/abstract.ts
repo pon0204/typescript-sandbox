ffe613faac493fd0af47ffb8e0306145f1c6583a
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-05-29 21:45:54 +0900

    feat: absctract

---
+++ practice-oop/src/polymorphism/abstract.ts
@@ -0,0 +1,20 @@
+abstract class Drink {
+  /** 抽象プロパティ */
+  abstract name: string
+  /** 抽象メソッド */
+  abstract comment(): void
+}
+
+// インスタンスの生成はできない
+// const drink = new Drink() // エラー
+
+class Coffee extends Drink {
+  name = 'コーヒー'
+  comment(): void {
+    console.log(`これは${this.name}です`)
+  }
+}
+
+const coffee = new Coffee()
+coffee.name = '珈琲'
+coffee.comment() // これはコーヒーです
