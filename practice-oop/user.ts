a15054528c7f1949f8060d444774d6551641d75e
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-05-29 15:41:55 +0900

    chore: 環境構築

---
+++ practice-oop/user.ts
@@ -0,0 +1,14 @@
+class User {
+  public name: string
+  constructor(name: string) {
+    this.name = name
+  }
+
+  formatText(): string {
+    return `name: ${this.name}`
+  }
+}
+
+const taro = new User('taro')
+console.log(taro.formatText())
+console.log(taro.name)
